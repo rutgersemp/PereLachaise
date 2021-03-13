a9195013c47def8b95c4865756b51f16edf0b5b6
Rutger Semp <r.semp@outlook.com>
2021-03-13 19:26:03 +0100

    Resolved merge error that deleted UART configs

--- config.h
+++ config.h
@@ -4,6 +4,18 @@
 
 # define DEG2RAD 0.0174532923716
 
+/*
+    UART configuration
+*/
+#define UART_ID uart0
+#define BAUD_RATE 115200
+#define DATA_BITS 8
+#define STOP_BITS 1
+#define PARITY    UART_PARITY_NONE
+
+#define UART_TX_PIN 0
+#define UART_RX_PIN 1
+
 const uint8_t __NUMBER_OF_LEGS__ = 3;
 const float __OMNI_DIAMETER_MM__ = 100.0;
 
