a253914893e1d38d48c47ed7052af51fbd072ec8
Rutger Semp <r.semp@outlook.com>
2021-03-13 15:47:45 +0100

    Added pin configs

--- config.h
+++ config.h
@@ -5,9 +5,17 @@
 const uint8_t __NUMBER_OF_LEGS__ = 3;
 const float __OMNI_DIAMETER_MM__ = 100.0;
 
-const uint8_t __LEGA_PWM_PIN__ = 17;
-const uint8_t __LEGA_DIR_PIN__ = 16;
+const uint8_t __LEGA_PWM_PIN__ = 16;
+const uint8_t __LEGA_DIR_PIN__ = 17;
 const float   __LEGA_ANGLE_RAD__ = 0.0;
+
+const uint8_t __LEGB_PWM_PIN__ = 21;
+const uint8_t __LEGB_DIR_PIN__ = 20;
+const float   __LEGB_ANGLE_RAD__ = 0.0;
+
+const uint8_t __LEGC_PWM_PIN__ = 27;
+const uint8_t __LEGC_DIR_PIN__ = 26;
+const float   __LEGC_ANGLE_RAD__ = 0.0;
 
 const uint8_t PIN_UNUSED = 2; // generic unconnected pin to pass as placeholder for future use
 
